d0c3d7ffb3f02079e625b16977f3db52733c1555
Electring <electring2025@gmail.com>
2026-03-26 14:24:58 +0530

    done-full

--- ODE_Solver.py
+++ ODE_Solver.py
@@ -1,15 +1,23 @@
+import math
+from cmath import sqrt
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy
 import scipy.integrate as solve_ivp
 
-time = (0, 10)
+g=9.81
+A_tank=8
+A_outlet=0.04
+initial_fuel_height=4.5
+
+time = (0, 150)
 
 def f(t, y):
-    dydt = np.sin(t)
+    dydt = -A_outlet*np.sqrt(np.abs(y[0]))*np.sqrt(2*g)/A_tank
     return dydt
 
-solution = scipy.integrate.solve_ivp(f, time, [-1], t_eval=np.linspace(0, 10, 100))
+solution = scipy.integrate.solve_ivp(f, time, [initial_fuel_height], t_eval=np.linspace(0, 150, 100))
 
 def plot(t, y):
     plt.plot(solution.t,solution.y[0])
