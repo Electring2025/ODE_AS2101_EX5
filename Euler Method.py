885e45db346eaadfe2e6dc13444ecff72bb87a38
Electring <electring2025@gmail.com>
2026-03-26 15:27:05 +0530

    Euler Method (final_final)

--- Euler Method.py
+++ Euler Method.py
@@ -9,6 +9,10 @@ initial_fuel_height = 4.5
 tf = 150
 time_step = 0.1
 
+def analytical(t, y):
+    y=(np.sqrt(initial_fuel_height)-A_outlet*np.sqrt(2*g)*t/(2*A_tank))**2
+    return y
+
 def euler_method (f, y0 , t):
 
     n = len (t)
@@ -32,13 +36,29 @@ for time_step in (2,0.5,0.1):
     iterations = int(np.floor(tf / time_step))
     t = np.linspace(0, tf, iterations)
     y = euler_method(my_ode, initial_fuel_height, t)
-    plt.plot(t, y, 'o-', markersize=30/iterations, mec='green', mfc='green', label=time_step)
+    plt.plot(t, y, 'o-', markersize=80/iterations, mec='black', mfc='black', label=time_step)
     plt.xlabel('time(s)')
     plt.ylabel('Height of fuel (m)')
     plt.title('Euler Method Solution:')
     savefig('Euler_Method_Solution_'+str(time_step)+'.png')
+
 plt . xlabel ('time(s)')
 plt . ylabel ('Height of fuel (m)')
 plt . title ('Euler Method Solution:')
 plt . grid ( True )
+plt.legend()
+savefig('Euler_Method_Solution_ALL')
+plt . show ()
+
+plt.close()
+
+iterations=int(np.floor(tf / time_step))
+t = np.linspace(0, tf, iterations)
+plt.plot(t,analytical(t,initial_fuel_height), 'r-', label='Analytical Solution')
+plt . xlabel ('time(s)')
+plt . ylabel ('Height of fuel (m)')
+plt . title ('Analytical Solution:')
+plt . grid ( True )
+plt.legend()
+savefig('Solution_Analytical')
 plt . show ()
